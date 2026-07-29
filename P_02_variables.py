ed9e0b5ea26897f76314fb99014efe1be24b56de
yaqubis50183-boop <yaqubis50183@masseyhigh.school.nz>
2026-07-30 10:43:33 +1200

    Add files via upload

---
+++ P_02_variables.py
@@ -0,0 +1,13 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+
+# Variables can be Boolean (True / False)
+love_programming = True
+
+# this variable has a problem!
+# My name = "Miss 6"
